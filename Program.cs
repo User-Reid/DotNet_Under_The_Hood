89f8f203c282e52b53cf866dcdf3b492e7571b48
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-09 07:44:00 -0500

    understanding value symantics and reference symantics.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Learned about .Net and C#
-// Learned about Common Intermediate Language (CIL)
-// Learned about Common Language Runtime (CLR)
-// learned about the stack and the heap
+﻿int number = 5;
+int anotherNumber = number;
+anotherNumber++;
+
+System.Console.WriteLine($"Number is {number}");
+System.Console.WriteLine($"another number is {anotherNumber}");
