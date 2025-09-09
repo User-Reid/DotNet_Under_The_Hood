d7eca7c846ca5b5a709ce9d3d8ca382ae1b56cc4
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-09 15:32:49 -0500

    reading from a file using StreamReader.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@ var writer = new FileWriter(filePath);
 writer.Write("Some text");
 writer.Write("Some other text");
 
+var reader = new SpecificLineFromTextFileReader(filePath);
+var third = reader.ReadLineNumber(3);
+var fourth = reader.ReadLineNumber(4);
+
+System.Console.WriteLine($"Third line is: {third}");
+System.Console.WriteLine($"Fourth line is: {fourth}");
+
 System.Console.WriteLine("Press any key to close.");
 Console.ReadKey();
 
@@ -17,5 +24,27 @@ public class FileWriter
   {
     _streamWriter.WriteLine(text);
     _streamWriter.Flush();
+  }
+}
+
+public class SpecificLineFromTextFileReader
+{
+  private readonly StreamReader _streamReader;
+  public SpecificLineFromTextFileReader(string filePath)
+  {
+    _streamReader = new StreamReader(filePath);
+  }
+
+  public string ReadLineNumber(int lineNumber)
+  {
+    _streamReader.DiscardBufferedData();
+    _streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+
+    for (var i = 0; i < lineNumber - 1; i++)
+    {
+      _streamReader.ReadLine();
+    }
+
+    return _streamReader.ReadLine();
   }
 }
