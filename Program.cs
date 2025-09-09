fe2c5bbc694c029d0fa5e2a619e943947c604cf0
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-08 19:24:05 -0500

    Learned about Common Intermediate Language

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // Learned about .Net and C#
+// Learned about Common Intermediate Language (CIL)
