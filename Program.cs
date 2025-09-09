1967512eaa8ceba9cc23331cc2a19c5063cfbc28
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-09 09:22:52 -0500

    completed coding ref exercise.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,44 @@
-﻿int number = 5;
+﻿// int number = 5;
 
-AddOneToNumber(ref number);
+// AddOneToNumber(ref number);
 
-System.Console.WriteLine(number);
+// System.Console.WriteLine(number);
 
-MethodWithOutParamater(out int otherNumber);
-System.Console.WriteLine($"Other number is {otherNumber}");
+// MethodWithOutParamater(out int otherNumber);
+// System.Console.WriteLine($"Other number is {otherNumber}");
+
+// Console.ReadKey();
+
+// void MethodWithOutParamater(out int number)
+// {
+//   number = 10;
+// }
+
+// void AddOneToNumber(ref int number)
+// {
+//   ++number;
+// }
+DateTime taco = new DateTime(2024, 08, 14);
+DateTime banana = new DateTime(2024, 08, 14);
+
+RefModifierFastForwardToSummerExercise.FastForwardToSummer(ref taco);
+
+System.Console.WriteLine(taco);
 
 Console.ReadKey();
 
-void MethodWithOutParamater(out int number)
+public static class RefModifierFastForwardToSummerExercise
 {
-  number = 10;
-}
-
-void AddOneToNumber(ref int number)
-{
-  ++number;
+  public static void FastForwardToSummer(ref DateTime x)
+  {
+    DateTime firstDayOfSummer = new DateTime(x.Year, 06, 21);
+    if (x > firstDayOfSummer)
+    {
+      System.Console.WriteLine("Neat");
+    }
+    else if (x < firstDayOfSummer)
+    {
+      x = firstDayOfSummer;
+    }
+  }
 }
