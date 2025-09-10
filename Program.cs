61180d3c78b232ed367004ddd3a000cc12f8a7d0
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-10 07:36:40 -0500

    going back to better understand

--- Program.cs
+++ Program.cs
@@ -1,34 +1,12 @@
-﻿const string path = @"C:\Users\aerma\Downloads\sampleData.csv";
-var data = new CsvReader().Read(path);
-
+﻿
 Console.ReadKey();
 
 public class CsvReader
 {
-  public CsvData Read(string path)
-  {
-    using var streamReader = new StreamReader(path);
-
-    const string Seperator = ",";
-    var columns = streamReader.ReadLine().Split(Seperator);
-
-    var rows = new List<string[]>();
-
-    while (!streamReader.EndOfStream)
-    {
-      var cellsInRow = streamReader.ReadLine().Split(Seperator);
-      rows.Add(cellsInRow);
-    }
-
-    return new CsvData(columns, rows);
-  }
-}
-
-public class CsvData
-{
   public string[] Columns { get; }
   public IEnumerable<string[]> Rows { get; }
-  public CsvData(string[] columns, IEnumerable<string[]> rows)
+
+  public CsvData CvsReader(string[] columns, IEnumerable<string[]> rows)
   {
     Columns = columns;
     Rows = rows;
--- obj/Debug/net8.0/DotNet_Under_The_Hood.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNet_Under_The_Hood.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNet_Under_The_Hood")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3731adb5af7905208d3047d1ce664af9b6421e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15101d90fdd99cd97e2bdc1dec36032f9108ff32")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNet_Under_The_Hood")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNet_Under_The_Hood")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
